006c7f304e32e2e63950ba21f38243333dc2111f
Paul Heasley <paul@phdesign.com.au>
2023-03-12 16:44:12 +1100

    Create user via react form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { trpc } from "../trpc";
 
 export const Form = () => {
-  return <div>Form</div>;
+  const [name, setName] = React.useState("");
+  const createUser = trpc.createUser.useMutation();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    createUser.mutate({ name });
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: 200,
+          gap: 10,
+        }}
+      >
+        {createUser.isLoading && <div>Creating user...</div>}
+        {createUser.error && <div>Error: {createUser.error.message}</div>}
+        {createUser.isSuccess && <div>User {createUser.data?.id} created!</div>}
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  );
 };
